fe8fe29a3ea742229381b771c9cec46ca795988d
Estultz731 <elizablue731@gmail.com>
2016-05-29 13:41:12 -0400

    changed footer and removed padding

--- js/main.js
+++ js/main.js
@@ -1,17 +1,24 @@
-function getAnimalSound(animal) {
-	animalSounds = {
-		'cat': 'meow',
-		'dog': 'woof',
-		'monkey': 'hoodeyhoo',
-		'cow': 'moo'
-	};
+( function( $, w, d ) {
 
-	for (var i = 0; i < animalSounds.length; i++) {
+	// On click of nav toggle
 
-		if( animalSounds[animal] === animal ) {
-			return 'Is a valid animal';
-		}else{
-			return 'That is not a valid animal!';
+	$(d).on('click', '#nav-toggle', function() {
+		
+		$('.nav-links').toggleClass('show-flex').toggle('slow');
+	});
+
+	$(w).on('resize', function() {
+
+		
+				
+		if(this.matchMedia( "(min-width: 0px) and (max-width: 767px) " ).matches) {
+
+			$('.nav-links').hide();
+
 		}
-	}
-}
+		if( this.matchMedia( "(min-width: 768px)" ).matches ) {
+			$('.nav-links').css('display', 'flex');
+			
+		}
+	});
+})( jQuery, window, document );
